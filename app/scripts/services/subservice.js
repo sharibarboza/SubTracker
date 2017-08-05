6d76d08698c1d1d055f9aeeaf16bb95a19003353
sharibarboza <barbozashari@gmail.com>
2017-08-04 19:51:28 -0600

    Refactor controllers and services

--- app/scripts/services/subservice.js
+++ app/scripts/services/subservice.js
@@ -8,14 +8,117 @@
  * Service in the tractApp.
  */
  angular.module('tractApp')
- .service('subService', function () {
+ .service('subService', ['$http', function ($http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
+    var baseUrl = 'http://www.reddit.com/user/';
+    var rawJson = 'raw_json=1';
+    var username;
+    var comments = [];
+    var submissions = [];
     var subs = {};
 
-    return {
-      organizeComments(comments) {
-        subs = {};
+    var resetData = function() {
+      comments = [];
+      submissions = [];
+      subs = {};
+    };
 
+    var getCommentPromise = function(username, after) {
+      var url = baseUrl+username+'/comments.json?'+rawJson+'&after='+after+'&limit=100';
+      return $http.get(url);
+    };
+
+    var getSubmitPromise = function(username, after) {
+      var url = baseUrl+username+'/submitted.json?'+rawJson+'&after='+after+'&limit=100';
+      return $http.get(url);
+    };
+
+    var getComments = function(username, after) {
+      if (after === 'first') {
+        return getCommentPromise(username, 0);
+      } else {
+        return getCommentPromise(username, after);
+      }
+    };
+
+    var getSubmitted = function(username, after) {
+      if (after === 'first') {
+        return getSubmitPromise(username, 0);
+      } else {
+        return getSubmitPromise(username, after);
+      }
+    };
+
+    var getData = function(response, where) {
+      if (response) {
+        var after = response.data.data.after;
+        var data = response.data.data.children;
+
+        for (var i = 0; i < data.length; i++) {
+          if (where === 'comments') {
+            comments.push(data[i]);
+          } else {
+            submissions.push(data[i]);
+          }
+        }
+
+        if (after) {
+          if (where === 'comments') {
+            return getComments(username, after);
+          } else {
+            return getSubmitted(username, after);
+          }
+
+        }
+      }
+      return null;
+    };
+
+    return {
+      setUser: function(user) {
+        username = user;
+        resetData();
+      },
+      setCommentList: function() {
+        var commentPromise = getComments(username, 'first');
+        var promise = commentPromise
+        .then(function(response) { return getData(response, 'comments'); })
+        .then(function(response) { return getData(response, 'comments'); })
+        .then(function(response) { return getData(response, 'comments'); })
+        .then(function(response) { return getData(response, 'comments'); })
+        .then(function(response) { return getData(response, 'comments'); })
+        .then(function(response) { return getData(response, 'comments'); })
+        .then(function(response) { return getData(response, 'comments'); })
+        .then(function(response) { return getData(response, 'comments'); })
+        .then(function(response) { return getData(response, 'comments'); })
+        .then(function(response) { return getData(response, 'comments'); })
+        .then(function(response) { return response; });
+        return promise;
+      },
+      setSubmitList: function() {
+        var submitPromise = getSubmitted(username, 'first');
+        var promise = submitPromise
+        .then(function(response) { return getData(response, 'submitted'); })
+        .then(function(response) { return getData(response, 'submitted'); })
+        .then(function(response) { return getData(response, 'submitted'); })
+        .then(function(response) { return getData(response, 'submitted'); })
+        .then(function(response) { return getData(response, 'submitted'); })
+        .then(function(response) { return getData(response, 'submitted'); })
+        .then(function(response) { return getData(response, 'submitted'); })
+        .then(function(response) { return getData(response, 'submitted'); })
+        .then(function(response) { return getData(response, 'submitted'); })
+        .then(function(response) { return getData(response, 'submitted'); })
+        .then(function(response) { return response; });
+        return promise;
+      },
+      getCommentList: function() {
+        return comments;
+      },
+      getSubmitList: function() {
+        return submissions;
+      },
+      organizeComments: function(comments) {
+        subs = {};
         // Push comments
         for (var i = 0; i < comments.length; i++) {
           var comment = comments[i].data;
@@ -39,7 +142,7 @@
           }
         }
       },
-      organizeSubmitted(submissions) {
+      organizeSubmitted: function(submissions) {
         for (var i = 0; i < submissions.length; i++) {
           var submission = submissions[i].data;
           var subreddit = submission.subreddit;
@@ -66,8 +169,8 @@
           }
         }
       },
-      getSubs() {
+      getSubs: function() {
         return subs;
       }
     };
-  });
+  }]);
--- app/scripts/services/userservice.js
+++ app/scripts/services/userservice.js
@@ -12,39 +12,19 @@
     // AngularJS will instantiate a singleton by calling "new" on this function
     var baseUrl = 'http://www.reddit.com/user/';
     var rawJson = 'raw_json=1';
+    var userPromise;
 
     var getUserPromise = function(username) {
       var url = baseUrl+username+'/about.json?'+rawJson;
-      return $http.get(url);
-    };
-
-    var getCommentPromise = function(username, after) {
-      var url = baseUrl+username+'/comments.json?'+rawJson+'&after='+after+'&limit=100';
-      return $http.get(url);
-    };
-
-    var getSubmitPromise = function(username, after) {
-      var url = baseUrl+username+'/submitted.json?'+rawJson+'&after='+after+'&limit=100';
-      return $http.get(url);
+      userPromise = $http.get(url);
     };
 
     return {
-      getAbout: function(username) {
-        return getUserPromise(username);
+      setUser: function(username) {
+        getUserPromise(username);
       },
-      getComments: function(username, after) {
-        if (after === 'first') {
-          return getCommentPromise(username, 0);
-        } else {
-          return getCommentPromise(username, after);
-        }
-      },
-      getSubmitted: function(username, after) {
-        if (after === 'first') {
-          return getSubmitPromise(username, 0);
-        } else {
-          return getSubmitPromise(username, after);
-        }
+      getUser: function() {
+        return userPromise;
       }
     };
   }]);
