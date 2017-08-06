24ef063fb3b17e3dcca6cfb9f29e215afc8e9820
sharibarboza <barbozashari@gmail.com>
2017-08-06 12:12:20 -0600

    Modify subreddits table display

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,7 @@
  */
  angular.module('tractApp')
  .controller('MainCtrl', ['$scope', function ($scope) {
- 	// Main will determine whether to display the search bar in the nav 
- 	// Should not be displayed on main page
+    // Main will determine whether to display the search bar in the nav 
+    // Should not be displayed on main page
     $scope.main = true;
 }]);
