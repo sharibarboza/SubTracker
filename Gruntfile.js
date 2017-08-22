5b302ab48b0148a65f34361fce3056a5765c1939
sharibarboza <barbozashari@gmail.com>
2017-08-22 01:32:11 -0600

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -338,7 +338,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'tractApp',
+          module: 'SubSnoopApp',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
