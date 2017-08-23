4eb14abee8f29a62bee33dbd1ddb32a1492a0b47
sharibarboza <barbozashari@gmail.com>
2017-08-22 22:37:25 -0600

    Modify text colors and icons

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,12 @@
  * Controller of the SubSnoopApp
  */
  angular.module('SubSnoopApp')
- .controller('MainCtrl', ['$scope', 'popularSubs', function ($scope, popularSubs) {
+ .controller('MainCtrl', ['$scope', 'popularSubs', 'newSubs', function ($scope, popularSubs, newSubs) {
     // Main will determine whether to display the search bar in the nav 
     // Should not be displayed on main page
     $scope.main = true;
 
     $scope.popularSubs = popularSubs;
+    $scope.newSubs = newSubs;
+    console.log(newSubs);
 }]);
