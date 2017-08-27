a7e1225888b6fa1f517113b1395022d8211f9ac9
sharibarboza <barbozashari@gmail.com>
2017-08-26 23:50:10 -0600

    Add config setup for Firebase deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -380,7 +380,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '*.html',
             'images/{,*/}*.{webp}',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'fonts/*.*'
           ]
         }, {
           expand: true,
