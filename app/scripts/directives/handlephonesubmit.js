c5ce7f3da6b9760a54f149803105bc275d3af15a
sharibarboza <barbozashari@gmail.com>
2017-09-03 02:16:03 -0600

    Refactor: change page to redirect variable for handle-phone-submit directive

--- app/scripts/directives/handlephonesubmit.js
+++ app/scripts/directives/handlephonesubmit.js
@@ -11,15 +11,17 @@ angular.module('SubSnoopApp')
 
     return {
       restrict: 'AE',
+      scope: {
+        redirect: '@'
+      },
       link: function postLink(scope, element, attrs) {
         var textFields = element.find('input');
-        var page = attrs.page;
 
         element.on('submit', function() {
           textFields[0].blur();
           var username = textFields[0].value;
 
-          if (page !== 'search') {
+          if (attrs.redirect === 'true') {
             $window.location.assign('#/' + username + '/');
             scope.$apply();
           }
