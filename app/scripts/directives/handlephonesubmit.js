9f450fdc8eb3c60416e207b004670c5da6259cc3
sharibarboza <barbozashari@gmail.com>
2017-09-03 00:49:45 -0600

    Change search to page in handle-phone-submit directive

--- app/scripts/directives/handlephonesubmit.js
+++ app/scripts/directives/handlephonesubmit.js
@@ -13,13 +13,13 @@ angular.module('SubSnoopApp')
       restrict: 'AE',
       link: function postLink(scope, element, attrs) {
         var textFields = element.find('input');
-        var page = attrs.userSearch;
+        var page = attrs.page;
 
         element.on('submit', function() {
           textFields[0].blur();
           var username = textFields[0].value;
 
-          if (search === 'true') {
+          if (page !== 'search') {
             $window.location.assign('#/' + username + '/');
             scope.$apply();
           }
