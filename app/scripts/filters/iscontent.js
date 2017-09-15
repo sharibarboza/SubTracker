838635c344753a1e46193e0f268c90ac6b5b16ed
sharibarboza <barbozashari@gmail.com>
2017-09-15 14:04:18 -0600

    Style: Add comments and fix indentation

--- app/scripts/filters/iscontent.js
+++ app/scripts/filters/iscontent.js
@@ -11,18 +11,28 @@
 angular.module('SubSnoopApp')
   .filter('isContent', function () {
 
-  	function isImage(url) {
-  		var formats = ['.png', '.jpg', '.gif'];
-  		for (var i = 0; i < formats.length; i++) {
-  			var format = formats[i];
-  			if (url.indexOf(format) >= 0) {
-  				return true;
-  			}
-  		}
-  		return false;
-  	}
+    /*
+     Returns true if url is a format ending in png, jpg, or gif
+    */
+    function isImage(url) {
+      var formats = ['.png', '.jpg', '.gif'];
+      for (var i = 0; i < formats.length; i++) {
+        var format = formats[i];
+        if (url.indexOf(format) >= 0) {
+          return true;
+        }
+      }
+      return false;
+    }
 
+    /*
+     Checks whether the content of a submission post has valid data
+     - Selftext_html/html: For self posts
+     - Media: For posts with embedded html, e.g. Youtube videos
+     - Preview: For link posts with attached images
+     - Spoiler posts can also contain images, therefore also check if the url is an image
+    */
     return function (input) {
-    	return input.selftext_html || input.html || input.media || input.preview || isImage(input.url);
+      return input.selftext_html || input.html || input.media || input.preview || isImage(input.url);
     };
   });
