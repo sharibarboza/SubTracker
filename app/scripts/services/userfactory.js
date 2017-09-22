28c82b3e1442dcea888ad1a66066574ce744ff0f
sharibarboza <barbozashari@gmail.com>
2017-09-22 14:15:33 -0600

    Style: Display sidebar icon images on smaller screens

--- app/scripts/services/userfactory.js
+++ app/scripts/services/userfactory.js
@@ -8,36 +8,17 @@
  * Factory in the SubSnoopApp.
  */
  angular.module('SubSnoopApp')
- .factory('userFactory', ['$http', '$sce', function ($http, $sce) {
-    var baseUrl = 'https://www.reddit.com/user/';
-    var rawJson = 'raw_json=1';
-    var user = "";
-
-    /*
-     Custom callback method for JSONP request
-    */
-    window.userCallback = function(response) {
-      user = response.data;
-    };
+ .factory('userFactory', ['$http', function ($http) {
 
     /*
      Makes a request to the Reddit API to fetch the user's data
     */
     return {
-      getData: function(username) {
-        // Return the promise
-        user = "";
-        var url = baseUrl+username+'/about.json?jsonp=userCallback';
-        var trustedUrl =  $sce.trustAsResourceUrl(url);
-        var promise = $http.jsonp(trustedUrl).then(function() {
-
-        }, function() {
-          return user;
+      getData: function (user) {
+        var url = 'https://api.reddit.com/user/' + user + '/about.json';
+        return $http.get(url).then(function(response) {
+          return response.data.data;
         });
-        return promise;
-      },
-      getUser: function() {
-        return user;
       }
     };
   }]);
