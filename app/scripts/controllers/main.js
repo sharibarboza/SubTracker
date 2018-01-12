de1ebb3d4f5f4fe94b20486b7ff2acb77b9aac49
sharibarboza <barbozashari@gmail.com>
2018-01-11 17:11:48 -0700

    Fix: Change footer year

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,9 @@
  * Controller of the SubSnoopApp
  */
  angular.module('SubSnoopApp')
- .controller('MainCtrl', ['$scope', 'popularSubs', 'newSubs', function ($scope, popularSubs, newSubs) {
+ .controller('MainCtrl', ['$scope', 'popularSubs', 'newSubs', 'moment', function ($scope, popularSubs, newSubs, moment) {
+ 	$scope.year = moment().year();
+
     // Main will determine whether to display the search bar in the nav 
     // Should not be displayed on main page
     $scope.main = true;
