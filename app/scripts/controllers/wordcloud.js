81348623781e49e96ddfb1c53f6765470802574c
sharibarboza <barbozashari@gmail.com>
2018-09-06 13:25:30 -0600

    Fix margins and borders for comments/submissions posts

--- app/scripts/controllers/wordcloud.js
+++ app/scripts/controllers/wordcloud.js
@@ -11,5 +11,5 @@ angular.module('SubSnoopApp')
   .controller('WordcloudCtrl', ['$scope', 'words', '$routeParams', function ($scope, words, $routeParams) {
     $scope.subreddit = $routeParams.subreddit;
     $scope.words = words.getWords($scope.subreddit, $routeParams.username);
-    $scope.colors = ["#2979ff", "#d4e157", "#ffca28", "#673ab7", "#fb8c00", "#67cad6", "#e64560"];
+    $scope.colors = ["#2979ff", "#d4e157", "#ffca28", "#673ab7", "#00C497", "#67cad6", "#ef5350"];
   }]);
